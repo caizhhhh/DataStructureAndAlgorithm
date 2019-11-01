8a6dbf320f3a481d93f2b00bdb01813618792ac7
caizh <15902075225@163.com>
2019-11-02 00:02:51 +0800

    update validate_stack_sequences

--- Stack/validate_stack_sequences.py
+++ Stack/validate_stack_sequences.py
@@ -6,9 +6,9 @@ from my_stack import Stack
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         stack = Stack()
+        index = 0
         for item in pushed:
             stack.push(item)
-            index = 0
             while popped and not stack.is_empty() and popped[index] == stack.get_top():
                 stack.pop()
                 index += 1
