9a9172fe4f82a45455db2c0bbf9a3739b6e71235
caizh <15902075225@163.com>
2019-11-20 19:39:02 +0800

    quict sort updates

--- sort&search/quick_sort.py
+++ sort&search/quick_sort.py
@@ -5,8 +5,17 @@ def quick_sort(data_list):
     # 中值
     middle_value = data_list[0]
     left, right = 1, len(data_list) - 1
+    left_stop, right_stop = False, False
     while left < right:
-        break
+        if not left_stop and data_list[left] > middle_value:
+            left_stop = True
+            left += 1
+        if not right_stop and data_list[right] < middle_value:
+            right_stop = True
+            right += 1
+        if left_stop and right_stop:
+            data_list[left], data_list[right] = data_list[right], data_list[left]
+            left_stop = right_stop = False
 
 
 def wtf(data_list):
